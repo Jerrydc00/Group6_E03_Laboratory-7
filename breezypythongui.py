7cdaa8d56fa7db960cbca17f8d9d45984e5d05b4
Jerry Dela Cruz <jerrydelacruz@mymail.mapua.edu.ph>
2025-02-12 17:07:02 +0800

    Update breezypythongui.py

--- breezypythongui.py
+++ breezypythongui.py
@@ -1,34 +1,4 @@
-"""
-File: breezypythongui.py
-Version: 1.2
-Copyright 2012, 2013, 2019 by Ken Lambert
 
-Resources for easy Python GUIs.
-
-LICENSE: This is open-source software released under the terms of the
-GPL (http://www.gnu.org/licenses/gpl.html).  Its capabilities mirror those 
-of BreezyGUI and BreezySwing, open-source frameworks for writing GUIs in Java,
-written by Ken Lambert and Martin Osborne.
-
-PLATFORMS: The package is a wrapper around Tkinter (Python 3.X) and should
-run on any platform where Tkinter is available.
-
-INSTALLATION: Put this file where Python can see it.
-
-RELEASE NOTES:
-Version 1.2 also now includes the class EasyCombobox for
-managing combo boxes (updated 08-15-2019).
-
-Version 1.2 also now supports the handling of selections in
-multiple list boxes (updated 08-15-2019).
-
-Version 1.2 also now includes the class EasyPanel, for organizing
-subpanes in windows and dialogs (updated 12-19-2012).
-
-Version 1.2 now also runs on either Python 3.x.x or
-Python 2.x.x (updated 2-4-2013).
-
-"""
 
 import sys
 versionNumber = sys.version_info.major
